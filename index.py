629ca0d9089277abdd9fb2f4f94b45a5adef1d41
thomas <bezille.thomasb@gmail.com>
2024-09-11 16:30:26 +0200

    📦 NEW: Creating the game's infinite loop

--- index.py
+++ index.py
@@ -2,3 +2,5 @@ import random
 
 words = "an chemin jour chose monde ecole etat pays probleme pomme systeme jeu pouvoir tete genre probleme argent nombre gouvernement programme membre voiture enfant corps visage autres niveau porte personne guerre regarder venir voir savoir obtenir faire etre garcon recherche parti histoire utiliser trouver travailler peut appeler sentir mettre garder tourner commencer arreter mener apprendre fournir arriver aimer deplacer courir jouer pourrait creer lire permettre offrir paraitre construire tomber couper atteindre rester seul vrai americain public grand petit différent haut juste vieux propre humain standard entier bon grand jeune important tot possible entier petit fin lourd gratuit special personnel majeur disponible"
 
+while True:
+    
